79615e38cc604715a81d2eacff786e51f20dcea0
LiNuX-Mallu <shaspachu457@gmail.com>
2023-12-30 13:28:43 +0530

    backup

--- server/server.ts
+++ server/server.ts
@@ -21,19 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: [`http://${CLIENT_HOST}:${CLIENT_PORT}`],
-    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "PATCH", "DELETE"],
-    credentials: true,
-  })
+	cors({
+		origin: [`http://${CLIENT_HOST}:${CLIENT_PORT}`],
+		methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "PATCH", "DELETE"],
+		credentials: true,
+	})
 );
 
 declare namespace NodeJS {
-  interface ProcessEnv {
-    PORT: number;
-    HOSTNAME: string;
-    MONGO_URI: string;
-  }
+	interface ProcessEnv {
+		PORT: number;
+		HOSTNAME: string;
+		MONGO_URI: string;
+	}
 }
 
 const server = http.createServer(app);
@@ -42,109 +42,109 @@ app.use("/api", router);
 
 //socket io
 const io = new Server(server, {
-  cors: {
-    origin: `http://${CLIENT_HOST}:${CLIENT_PORT}`,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-  transports: ["websocket", "polling"],
-  allowEIO3: true,
+	cors: {
+		origin: `http://${CLIENT_HOST}:${CLIENT_PORT}`,
+		methods: ["GET", "POST"],
+		credentials: true,
+	},
+	transports: ["websocket", "polling"],
+	allowEIO3: true,
 });
 
 //chat and video configuration
 io.on("connection", async (socket: Socket) => {
-  socket.on("joinApp", (username: string) => {
-    socket.join(username);
-  });
-  socket.on("leaveApp", (username: string) => {
-    socket.leave(username);
-  });
+	socket.on("joinApp", (username: string) => {
+		socket.join(username);
+	});
+	socket.on("leaveApp", (username: string) => {
+		socket.leave(username);
+	});
 
-  socket.on("joinChat", (name: string) => {
-    socket.join(name);
-  });
-  socket.on("leaveChat", (name: string) => {
-    socket.leave(name);
-  });
+	socket.on("joinChat", (name: string) => {
+		socket.join(name);
+	});
+	socket.on("leaveChat", (name: string) => {
+		socket.leave(name);
+	});
 
-  //video call
-  socket.on("requestVideoCall", (data: { from: string; to: string }) => {
-    io.to(data.to).emit("receiveVideoCallRequest", data.from);
-  });
-  socket.on("rejectVideoCall", (data: { from: string; to: string }) => {
-    io.to(data?.to + data?.from).emit("receiveVideoCallRejection", data?.from);
-  });
-  socket.on("acceptVideoCall", (data: { from: string; to: string }) => {
-    io.to(data.to).emit("receiveVideoCallAcceptance", data?.from);
-  });
-  socket.on("sendStopCall", (data: { from: string; to: string }) => {
-    io.to(data?.to).emit("receiveStopCall", data?.from);
-  });
-  socket.on("sendEndCall", (data: { from: string; to: string }) => {
-    io.to(data?.to).emit("receiveEndCall", data?.from);
-  });
+	//video call
+	socket.on("requestVideoCall", (data: { from: string; to: string }) => {
+		io.to(data.to).emit("receiveVideoCallRequest", data.from);
+	});
+	socket.on("rejectVideoCall", (data: { from: string; to: string }) => {
+		io.to(data?.to + data?.from).emit("receiveVideoCallRejection", data?.from);
+	});
+	socket.on("acceptVideoCall", (data: { from: string; to: string }) => {
+		io.to(data.to).emit("receiveVideoCallAcceptance", data?.from);
+	});
+	socket.on("sendStopCall", (data: { from: string; to: string }) => {
+		io.to(data?.to).emit("receiveStopCall", data?.from);
+	});
+	socket.on("sendEndCall", (data: { from: string; to: string }) => {
+		io.to(data?.to).emit("receiveEndCall", data?.from);
+	});
 
-  interface Data {
-    offer?: RTCSessionDescription;
-    answer?: RTCSessionDescription;
-    candidate?: RTCIceCandidate;
-    to: string;
-  }
+	interface Data {
+		offer?: RTCSessionDescription;
+		answer?: RTCSessionDescription;
+		candidate?: RTCIceCandidate;
+		to: string;
+	}
 
-  socket.on("sendSignal", (data: Data) => {
-    io.to(data.to).emit("receiveSignal", data);
-  });
+	socket.on("sendSignal", (data: Data) => {
+		io.to(data.to).emit("receiveSignal", data);
+	});
 
-  //messaging
-  socket.on(
-    "sendMessage",
-    (message: {
-      message: MessageInterface;
-      to: string;
-      chatId: string | null;
-    }) => {
-      io.to(message.to + message?.message?.sender).emit(
-        "receiveMessage",
-        message.message
-      );
-      try {
-        saveMessage(
-          message?.chatId,
-          message?.message?.sender,
-          message?.to,
-          message.message
-        ).then((res: string | null | undefined) => {
-          if (res) {
-            io.to(message?.message?.sender + message?.to).emit(
-              "receiveChatId",
-              res
-            );
-            io.to(message?.to + message?.message?.sender).emit(
-              "receiveChatId",
-              res
-            );
-          }
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  );
-  socket.on(
-    "typing",
-    (data: { username: string; me: string; flag: boolean }) => {
-      io.to(data?.username + data?.me).emit("isTyping", data.flag);
-    }
-  );
+	//messaging
+	socket.on(
+		"sendMessage",
+		(message: {
+			message: MessageInterface;
+			to: string;
+			chatId: string | null;
+		}) => {
+			io.to(message.to + message?.message?.sender).emit(
+				"receiveMessage",
+				message.message
+			);
+			try {
+				saveMessage(
+					message?.chatId,
+					message?.message?.sender,
+					message?.to,
+					message.message
+				).then((res: string | null | undefined) => {
+					if (res) {
+						io.to(message?.message?.sender + message?.to).emit(
+							"receiveChatId",
+							res
+						);
+						io.to(message?.to + message?.message?.sender).emit(
+							"receiveChatId",
+							res
+						);
+					}
+				});
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	);
+	socket.on(
+		"typing",
+		(data: { username: string; me: string; flag: boolean }) => {
+			io.to(data?.username + data?.me).emit("isTyping", data.flag);
+		}
+	);
 });
 
 mongoConnect(MONGO_URI!)
-  .then(() => {
-    console.log("connected to database");
-    server.listen(typeof PORT === "number" ? PORT : 3000, HOST!, () => {
-      console.log(`Server listening at http://${HOST}:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("failed connecting to database\n", error);
-  });
+	.then(() => {
+		console.log("connected to database");
+		server.listen(typeof PORT === "number" ? PORT : 3000, HOST!, () => {
+			console.log(`Server listening at http://${HOST}:${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log("failed connecting to database\n", error);
+	});
