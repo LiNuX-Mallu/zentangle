72cd4ea6cb5513aaad3f73a478b42c8a3de1db17
LiNuX-Mallu <shaspachu457@gmail.com>
2023-12-31 13:05:30 +0530

    fix env

--- client/src/instances/urls.ts
+++ client/src/instances/urls.ts
@@ -1,2 +1,5 @@
-export const server = "http://localhost:3000";
-export const ApiUrl = server+"/api";
+const API_URL = import.meta.env.VITE_API_URL;
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+export const server = SERVER_URL;
+export const ApiUrl = API_URL;
