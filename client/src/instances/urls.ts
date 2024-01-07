693d8fd4ad0ee15d4ec1b99b2ead9528e9046d6f
LiNuX-Mallu <shaspachu457@gmail.com>
2024-01-07 15:05:14 +0530

    fix cors

--- client/src/instances/urls.ts
+++ client/src/instances/urls.ts
@@ -1,5 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL;
-const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
+// const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
-export const server = SERVER_URL;
-export const ApiUrl = API_URL;
+export const server = "https://zentangle-2-tdo2clfghq-de.a.run.app";
+export const ApiUrl = "https://zentangle-2-tdo2clfghq-de.a.run.app/api";
