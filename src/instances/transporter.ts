ee140275b41366aa4f93cf835be79aeaf54488a3
LiNuX-Mallu <shaspachu457@gmail.com>
2024-01-10 14:31:05 +0530

    fix

--- src/instances/transporter.ts
+++ src/instances/transporter.ts
@@ -6,10 +6,15 @@ dotenv.config();
 let { EMAIL, EMAIL_PASSWORD } = process.env;
 
 export default nodemailer.createTransport({
+  host: "smtp.gmail.com",
   service: 'gmail',
+  port: 587,
   secure: false,
   auth: {
     user: EMAIL,
     pass: EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  }
 });
